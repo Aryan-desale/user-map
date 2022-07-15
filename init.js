76151177397761ef615ba6c089787168bd5fab3e
aryandesale <aryandesale04@gmail.com>
2022-07-15 17:02:26 +0530

    second commit

---
+++ init.js
@@ -0,0 +1,8 @@
+window.process = {
+  env: {
+    APP_ID: 'NWDH5BRXYO',
+    CLIENT_SECRET: 'c2b0d7576ed869cc13d2ba024c1b6dcf',
+    ACCESS_TOKEN:
+      'pk.eyJ1IjoiYXJ5YW4wMDA3IiwiYSI6ImNrczA3YXR3cjFoOWwzMW83eGh5bndsMXUifQ.goNKVbDPWtlc4i4h-Imj1w',
+  },
+};
