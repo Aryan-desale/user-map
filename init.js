919c62d291902a77536227a255112649862bf99d
aryandesale <aryandesale04@gmail.com>
2022-07-15 17:26:46 +0530

    third commit

--- init.js
+++ init.js
@@ -3,6 +3,6 @@ window.process = {
     APP_ID: 'NWDH5BRXYO',
     CLIENT_SECRET: 'c2b0d7576ed869cc13d2ba024c1b6dcf',
     ACCESS_TOKEN:
-      'pk.eyJ1IjoiYXJ5YW4wMDA3IiwiYSI6ImNrczA3YXR3cjFoOWwzMW83eGh5bndsMXUifQ.goNKVbDPWtlc4i4h-Imj1w',
+      'pk.eyJ1IjoiYXJ5YW4wMDA3IiwiYSI6ImNsNW1lNDJ3MTAwbnEzYnA4aGs3aHF2czUifQ.-8fLmsg5rBk1nq2XkzJfsQ',
   },
 };
