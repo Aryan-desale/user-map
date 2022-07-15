7b26b2ffd6aa2a2741b902285482dd39b3aa0d45
aryandesale <aryandesale04@gmail.com>
2022-07-15 16:36:39 +0530

    first commit

--- main.js
+++ main.js
@@ -1,239 +1,166 @@
 let userNameArr = [];
 
-let userPhoto = []
+let userPhoto = [];
 
-let userDesignation = []
+let userDesignation = [];
 
 let parentDiv = document.querySelector('.parent-div');
 
-let userCompany = []
-let lat = []
+let userCompany = [];
+let lat = [];
 
-let lng = []
+let lng = [];
+
+let APP_ID = process.env.APP_ID;
+
+let CLIENT_SECRET = process.env.CLIENT_SECRET;
+
+let ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 
 let searchBox = document.querySelector('#mySearch');
 
-
-
 const search = instantsearch({
-    indexName: 'user_info',
-    searchClient: algoliasearch('NWDH5BRXYO', 'c2b0d7576ed869cc13d2ba024c1b6dcf'),
-  });
+  indexName: 'user_info',
+  searchClient: algoliasearch(`${APP_ID}`, `${CLIENT_SECRET}`),
+});
 
-  search.addWidgets([
-    instantsearch.widgets.searchBox({
-      container: '#searchbox',
-    }),
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+  }),
 
-    instantsearch.widgets.hits({
-        container: document.querySelector('.data-div'),
-        templates: {
-          item: function(data) {
-  
+  instantsearch.widgets.hits({
+    container: document.querySelector('.data-div'),
+    templates: {
+      item: function (data) {
+        userNameArr.push(data.fullName);
 
-            userNameArr.push(data.fullName)
+        userPhoto.push(data.photo);
 
-            userPhoto.push(data.photo)
+        userDesignation.push(data.designation);
 
-            userDesignation.push(data.designation)
+        lat.push(data['location.lat']);
 
-            lat.push(data['location.lat'])
+        userCompany.push(data.companyName);
 
-            userCompany.push(data.companyName);
+        lng.push(data['location.lng']);
 
-            lng.push(data['location.lng'])
+        if (lng.length > 999) {
+          setTimeout(createDiv, 1000);
+        }
+      },
+    },
+  }),
+]);
 
-            if(lng.length > 999) {
+search.start();
 
-              setTimeout(createDiv, 1000)
-              
-            }
-            
-
-
-      
-           }
-         },
-  
-     
-      }),
-   
-    
-  ]);
-
-
-  
-
-  
-  
-
-  search.start();
-
-
-
-  function createDiv() {
-
-  for(let i = 0; i < userNameArr.length; i++) {
-
-    
+function createDiv() {
+  for (let i = 0; i < userNameArr.length; i++) {
     let mainDiv = document.createElement('div');
 
-    mainDiv.userName = `mainDiv${i}`
-    
+    mainDiv.userName = `mainDiv${i}`;
+
     let infoDiv = document.createElement('div');
 
-    infoDiv.userName = `infoDiv${i}`
-    
+    infoDiv.userName = `infoDiv${i}`;
+
     let image = document.createElement('img');
 
-    image.userName = `image${i}`
+    image.userName = `image${i}`;
 
-    
-    let userName = document.createElement('h2')
+    let userName = document.createElement('h2');
 
-    userName.userName = `userName${i}`
+    userName.userName = `userName${i}`;
 
-
-    
     let userField = document.createElement('span');
 
-    userField.userName = `userField${i}`
+    userField.userName = `userField${i}`;
 
-    
-    
     mainDiv.className = `popup${i}`;
 
     mainDiv.classList.add('popup');
 
-
     image.className = `img${i}`;
-    
-    image.classList.add('img');
-    
-    infoDiv.className = `info${i}`;
-    
-    userName.className = `userName${i}`
 
-    userName.classList.add('heading')
+    image.classList.add('img');
+
+    infoDiv.className = `info${i}`;
+
+    userName.className = `userName${i}`;
+
+    userName.classList.add('heading');
 
     userName.innerHTML = userNameArr[i];
-    
-    
+
     infoDiv.appendChild(userName);
-    
+
     infoDiv.appendChild(userField);
-    
-    mainDiv.appendChild(image)
-    
-    mainDiv.appendChild(infoDiv)
-    
-    parentDiv.appendChild(mainDiv)
 
+    mainDiv.appendChild(image);
 
-    if(userDesignation[i].length !== 0 ) {
+    mainDiv.appendChild(infoDiv);
 
+    parentDiv.appendChild(mainDiv);
+
+    if (userDesignation[i].length !== 0) {
       userField.innerHTML = userDesignation[i];
-
     }
-   
 
-    if(userPhoto[i].length !== 0) {
-
+    if (userPhoto[i].length !== 0) {
       document.querySelector(`.img${i}`).src = userPhoto[i];
-
     }
 
-  
-
-    if(i > '998') {
-
-
+    if (i > '998') {
       setTimeout(setPopup, 1000);
-
-
     }
-    
-    
-    
-    
-    
-    
-    
-    }
-
   }
-
-
-let arr =[];
-
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiYXJ5YW4wMDA3IiwiYSI6ImNrczA3YXR3cjFoOWwzMW83eGh5bndsMXUifQ.goNKVbDPWtlc4i4h-Imj1w';
-const map = new mapboxgl.Map({
-container: 'map', // container ID
-style: 'mapbox://styles/mapbox/streets-v11', 
-center: [-74.5, 40],
-zoom: 1,
-});
-
-
-
-
-
-
-
-function setPopup() {
-
-let fakearr = [];
-
-
-  for(let i= 0; i < userNameArr.length; i++) {
-
-    arr[i] = document.querySelector(`.popup${i}`);
-
-
-    if(lat.length == 0 && lng.length == 0) {
-
-      return
-    }
-    else {
-    fakearr[i] = new mapboxgl.Popup({ closeButton: false, closeOnClick: false })
-    .setLngLat([lng[i], lat[i]])
-    .setDOMContent(arr[i] )
-    .setMaxWidth("200px")
-    .addTo(map);
-    }
-
-
-
-  }
-
-
 }
 
+let arr = [];
 
-
- 
-map.on('style.load', () => {
-
-map.addControl(new mapboxgl.NavigationControl());   
-
+mapboxgl.accessToken = `${CLIENT_SECRET}`;
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [-74.5, 40],
+  zoom: 1,
 });
 
+function setPopup() {
+  let fakearr = [];
 
+  for (let i = 0; i < userNameArr.length; i++) {
+    arr[i] = document.querySelector(`.popup${i}`);
+
+    if (lat.length == 0 && lng.length == 0) {
+      return;
+    } else {
+      fakearr[i] = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false,
+      })
+        .setLngLat([lng[i], lat[i]])
+        .setDOMContent(arr[i])
+        .setMaxWidth('200px')
+        .addTo(map);
+    }
+  }
+}
+
+map.on('style.load', () => {
+  map.addControl(new mapboxgl.NavigationControl());
+});
 
 searchBox.addEventListener('keyup', () => {
-
   let value = searchBox.value;
 
-  for(let i = 0; i < userNameArr.length; i++) {
+  for (let i = 0; i < userNameArr.length; i++) {
+    let name = document.querySelector(`.userName${i}`).innerHTML;
 
-    let name =  document.querySelector(`.userName${i}`).innerHTML;
-
-    name = name.toLowerCase()
-
+    name = name.toLowerCase();
 
     if (name.indexOf(`${value}`) == -1) {
-
       let childDiv = document.querySelector(`.popup${i}`);
 
       let parentDiv = childDiv.parentNode;
@@ -241,79 +168,44 @@ searchBox.addEventListener('keyup', () => {
       let content = parentDiv.closest('.mapboxgl-popup');
 
       content.style.display = 'none';
-
-
-
-
     }
 
-
-    if(value.length == 0) {
-      for(let i = 0; i <= 10; i++) {
-
+    if (value.length == 0) {
+      for (let i = 0; i <= 10; i++) {
         let childDiv = document.querySelector(`.popup${i}`);
 
         let parentDiv = childDiv.parentNode;
-  
-  
+
         let content = parentDiv.closest('.mapboxgl-popup');
 
-         content.style.display = 'flex';
-
-
+        content.style.display = 'flex';
       }
     }
-
-
-
-
   }
-
-})
+});
 
 searchBox.addEventListener('keydown', (event) => {
-
-
   let key = event.keyCode || event.charCode || event.which;
 
+  if (key == 8 || key == 46) {
+    let value = searchBox.value;
 
-    if( key == 8 || key == 46 ) {
+    value = value.slice(0, -1);
 
-      let value = searchBox.value;
+    for (let i = 0; i < userNameArr.length; i++) {
+      let name = document.querySelector(`.userName${i}`).innerHTML;
 
-      value = value.slice(0, -1)
+      name = name.toLowerCase();
 
+      if (name.indexOf(`${value}`) > -1) {
+        let childDiv = document.querySelector(`.popup${i}`);
 
-      for(let i = 0; i < userNameArr.length; i++) {
+        let parentDiv = childDiv.parentNode;
 
-            let name =  document.querySelector(`.userName${i}`).innerHTML;
+        let content = parentDiv.closest('.mapboxgl-popup');
 
-             name = name.toLowerCase()
-
-
-                if (name.indexOf(`${value}`) > -1) {
-
-                  let childDiv = document.querySelector(`.popup${i}`);
-
-                  let parentDiv = childDiv.parentNode;
-
-                  let content = parentDiv.closest('.mapboxgl-popup');
-
-                  content.style.display = 'flex';
-
+        content.style.display = 'flex';
       }
-
-  }
-
     }
-
-  
-
-
-
-
-
-
-
-})
-
+  }
+});
